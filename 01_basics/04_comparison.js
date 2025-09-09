abe4a98198655e9c805b487cebbd6cdae6edade6
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2025-09-09 18:52:55 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2==1)
+//console.log(2<1)
+//console.log(2!=1)
+
+
+//while comparing to values it is good if both the variables have same data types 
+//console.log("2">1)
+//console.log("02">1)
+
+//console.log(null>0)//false
+//console.log(null==0)//false
+//console.log(null>=0)//true
+
+console.log(undefined>0)
+console.log(undefined>=0)
+console.log(undefined==0)
