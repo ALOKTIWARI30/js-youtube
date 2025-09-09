451aac7a1fdc764595ef0254094a75c33c42dc7f
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2025-09-09 18:22:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,30 @@
+let score = "33" 
+let score2="33abc"
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueinNumber=Number(score)
+let valueinNumber2=Number(score2)
+//console.log(typeof(valueinNumber))
+//console.log(valueinNumber2)
+
+//"33"=>33
+//"33abc"=>NaN
+// "null"=>0
+//unefined=>"NaN"
+//"Alok" =>NaN
+//true/false=>1/0
+let isLoggedIn=1
+let BooleanisLoggedIn=Boolean(isLoggedIn)
+//console.log(typeof(BooleanisLoggedIn))
+//console.log(BooleanisLoggedIn)
+
+//1=>true
+//0=>false
+//""=>false
+//"Alok"=>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber))
