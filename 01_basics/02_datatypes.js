e6303e532dce27edafd94c0b56424127ab17497c
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2025-09-09 18:03:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("Alok")
+
+/*console.log(3 //code readability should be high
+
+    +3
+    */
+
+    let name ="Alok"
+    let age=18
+    let IsLoggedIn= false
+
+    //number => 2 to power 53
+    //bigInt
+    //string // '' " " you can use both single and double quotes
+    //boolean=>true or false
+    //null =>stand alone value
+    //undefined =>
+     //symbol //to find uniqueness
+
+    //object
+
+   // console.log(typeof "Alok")
+   // console.log(typeof age)
+   console.log(typeof null)
+   console.log(typeof undefined)
+    
+
