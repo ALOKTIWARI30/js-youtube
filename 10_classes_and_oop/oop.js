e065b489bf0372f53f4ca508a60dbc5756c1a1db
ALOKTIWARI30 <aloktiwari200345@gmail.com>
2025-09-30 21:39:33 +0000

    oops

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,10 @@
+const user={
+    usename:"Alok",
+    logincount:8,
+    Signedin:true,
+    getuserdeatils:function(){
+        console.log("got user details from database")
+    }
+}
+ console.log(user.username)
+ console.log(user.getuserdetails())
